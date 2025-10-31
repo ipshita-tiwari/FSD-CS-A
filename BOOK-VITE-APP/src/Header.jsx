8a539f80237b6176a6e71e021653cfa839a01366
Ipshita Tiwari <ipshita.tiwari15@gmail.com>
2025-10-31 13:01:47 +0530

    BOOK-VITE-APP added

---
+++ BOOK-VITE-APP/src/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import '/.Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+        <h1> <center>BOOK STORE</center></h1>
+    </div>
+  )
+}
+
+export default Header
